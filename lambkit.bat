8e642e62448bbc72ecd0988c64ad52e0a58a06d5
DELL <DELL@DESKTOP-DK1FNRL>
2019-11-14 13:46:36 +0800

    升级JFinal到4.7

--- lambkit.bat
+++ lambkit.bat
@@ -19,19 +19,51 @@ setlocal & pushd
 
 
 rem 启动入口类,该脚本文件用于别的项目时要改这里
-set MAIN_CLASS=com.lambkit.Application
+set MAIN_CLASS=com.lambkit.LambkitApplication
 
 rem Java 命令行参数,根据需要开启下面的配置,改成自己需要的,注意等号前后不能有空格
 rem set "JAVA_OPTS=-Xms256m -Xmx1024m -Dundertow.port=80 -Dundertow.host=0.0.0.0"
 rem set "JAVA_OPTS=-Dundertow.port=80 -Dundertow.host=0.0.0.0"
 
+
+if "%1"=="start" goto normal
+if "%1"=="stop" goto normal
+if "%1"=="restart" goto normal
+
+goto error
+
+
+:error
+echo Usage: lambkit.bat start | stop | restart
+goto :eof
+
+
+:normal
+if "%1"=="start" goto start
+if "%1"=="stop" goto stop
+if "%1"=="restart" goto restart
+goto :eof
+
+
+:start
 set APP_BASE_PATH=%~dp0
 set CP=%APP_BASE_PATH%config;%APP_BASE_PATH%lib\*
+echo starting lambkit undertow
 java -Xverify:none %JAVA_OPTS% -cp %CP% %MAIN_CLASS%
+goto :eof
 
+
+:stop
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+echo stopping lambkit undertow
+for /f "tokens=1" %%i in ('jps -l ^| find "%MAIN_CLASS%"') do ( taskkill /F /PID %%i )
+goto :eof
+
+
+:restart
+call :stop
+call :start
+goto :eof
 
 endlocal & popd
 pause
-
-
-
