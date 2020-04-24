98569becbc8994e42c4528eda0c93b54ba294ad8
孤竹行 <gismail@foxmail.com>
2020-04-24 11:18:14 +0800

    1.0.3版

--- src/main/java/com/lambkit/common/LambkitConsts.java
+++ src/main/java/com/lambkit/common/LambkitConsts.java
@@ -20,7 +20,7 @@ package com.lambkit.common;
  */
 public class LambkitConsts {
 
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 	
     public static final String TOKEN = "lambkit-token";
 	/**
